59b76507987fd740c08f450b0c8c606c200c6531
Urahs <sharumaku@gmail.com>
2023-02-12 18:18:43 +0300

    ...

--- app/src/main/java/com/example/app/MainActivity.kt
+++ app/src/main/java/com/example/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.drawingapp
+package com.example.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
--- app/src/test/java/com/example/app/ExampleUnitTest.kt
+++ app/src/test/java/com/example/app/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.example.drawingapp
+package com.example.app
 
 import org.junit.Test
 
